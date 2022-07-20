9495278b713725decbecd2f6f325a67a21e192f6
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-07-20 12:23:01 -0600

    Cambio a interfaz y objetos dropdown, creacion de interfaz Factura, habilitar rutas mvc, llamado de DbContext en facturacontroller, cambio de icono a pagina.

--- IPSAInventario/App_Start/RouteConfig.cs
+++ IPSAInventario/App_Start/RouteConfig.cs
@@ -13,6 +13,18 @@ namespace IPSAInventario
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Habilitando atributos
+            routes.MapMvcAttributeRoutes();
+            //Ruta Statica
+            /*routes.MapRoute(
+                "Inicio",
+                "factura/index",
+                new { controller = "Factura", action = "index" });
+            /*"MoviesByReleaseDate",
+            "movies/released/{year}/{month}",
+            new { controller = "Factura", action = "index" },
+            new { year = @"2015|2016", month = @"\d{2}" });*/
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ IPSAInventario/Controllers/FacturaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPSAInventario.Controllers
+{
+    public class FacturaController : Controller
+    {
+        private InventarioDbcontext _context;
+
+        public FacturaController()
+        {
+            _context = new InventarioDbcontext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Factura
+        public ActionResult Index()
+        {
+            var factura = _context.Factura.ToList();
+            return View(factura);
+        }
+    }
+}
