661c90e440fa531499e1c52e57e466500be0dadf
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-07-25 08:56:16 -0600

    Poblando tabla Proveedores

---
+++ IPSAInventario/Migrations/202207251451199_PoblandoProveedores.cs
@@ -0,0 +1,19 @@
+﻿namespace IPSAInventario.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PoblandoProveedores : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Proveedores VALUES ('Juan')");
+            Sql("INSERT INTO Proveedores VALUES ('Pepe')");
+            Sql("INSERT INTO Proveedores VALUES ('Felipe')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
