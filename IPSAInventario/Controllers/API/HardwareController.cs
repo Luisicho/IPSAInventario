459eedf2a5ac4f76162ba2ca6d5543edb201e7af
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-08 12:13:00 -0600

    api deleteHardware

--- IPSAInventario/Controllers/API/HardwareController.cs
+++ IPSAInventario/Controllers/API/HardwareController.cs
@@ -37,5 +37,21 @@ namespace IPSAInventario.Controllers.API
                 return NotFound();
             return Ok(Mapper.Map<Hardware, HardwareDto>(hardwareInDB));
         }
+        //DELETE /api/hardware/1
+        [HttpDelete]
+        public IHttpActionResult DeleteHardware(int id)
+        {
+            //Consulta el modelo con id x
+            var hardwareInDB = _context.Hardware.SingleOrDefault(h => h.IDHardware == id);
+            //Valida si el modelo existe
+            if (!ModelState.IsValid)
+                return NotFound();
+            //Elimina modelo de DB
+            _context.Hardware.Remove(hardwareInDB);
+            //Actualiza el modelo en DB
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
