ce15f3bf4b60c4d382d2e38dc756797129903011
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-09 10:03:57 -0600

    AddControllerComputadoraAndDBconnection

---
+++ IPSAInventario/Controllers/ComputadoraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IPSAInventario.ViewModels;
+using IPSAInventario.Models;
+
+namespace IPSAInventario.Controllers
+{
+    public class ComputadoraController : Controller
+    {
+        //VARIABLES
+        private InventarioDbcontext _context;//Base de datos
+
+        //Inicia la base de datos para sus consulta
+        public ComputadoraController()
+        {
+            _context = new InventarioDbcontext();
+        }
+        //Cierra la conexion a la base de datos
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Computadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
