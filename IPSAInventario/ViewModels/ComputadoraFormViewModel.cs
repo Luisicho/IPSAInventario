d6a4121c0d810e40c7bb8c40ac4e8644e1ca57e5
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-09 10:08:49 -0600

    addElementsToComputadoraFormViewModel

--- IPSAInventario/ViewModels/ComputadoraFormViewModel.cs
+++ IPSAInventario/ViewModels/ComputadoraFormViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using IPSAInventario.Models;
 
 namespace IPSAInventario.ViewModels
@@ -14,7 +16,40 @@ namespace IPSAInventario.ViewModels
         }
         public ComputadoraFormViewModel(Computadora computadora)
         {
-
+        
         }
+        [StringLength(7)]
+        public string Codigo_PC { get; set; }
+
+        public bool? Actualizado { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? Baja { get; set; }
+
+        public string Aplicacion { get; set; }
+
+        public string Expediente { get; set; }
+
+        public bool? Check_ { get; set; }
+
+        public string Maquina { get; set; }
+
+        public bool? Red { get; set; }
+
+        public string IPV4 { get; set; }
+
+        public string Mascara_IPV4 { get; set; }
+
+        public string IPV6 { get; set; }
+
+        public string Mascara_IPV6 { get; set; }
+
+        public string Internet { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Tipo_Computador { get; set; }
+
+        public string Observaciones { get; set; }
     }
 }
