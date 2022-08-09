2a14c4918b1c27e5655231b85eb6bb0865600447
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-09 10:06:57 -0600

    addConstructorToComputadoraFormViewModel

--- IPSAInventario/ViewModels/ComputadoraFormViewModel.cs
+++ IPSAInventario/ViewModels/ComputadoraFormViewModel.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using IPSAInventario.Models;
 
 namespace IPSAInventario.ViewModels
 {
     public class ComputadoraFormViewModel
     {
+        public ComputadoraFormViewModel()
+        {
+
+        }
+        public ComputadoraFormViewModel(Computadora computadora)
+        {
+
+        }
     }
 }
