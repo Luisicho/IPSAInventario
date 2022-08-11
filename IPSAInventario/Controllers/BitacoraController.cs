cb5054635fe0e01fc8a5f0d6056746d6fa11063a
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-11 12:37:42 -0600

    addBitacoraController and view

---
+++ IPSAInventario/Controllers/BitacoraController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IPSAInventario.Models;
+
+namespace IPSAInventario.Controllers
+{
+    public class BitacoraController : Controller
+    {
+        //VARIABLES
+        private InventarioDbcontext _context;//Base de datos
+
+        //Inicia la base de datos para sus consulta
+        public BitacoraController()
+        {
+            _context = new InventarioDbcontext();
+        }
+        //Cierra la conexion a la base de datos
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // Bitacora/Index/id(Codigo_PC)
+        [HttpGet]
+        public ActionResult Index(string id)
+        {
+            //var Listabitacoras = _context.Bitacora.Select(b => b.Codigo_PC == id);
+            var bitacora = new Bitacora();
+            bitacora.Codigo_PC = id;
+            return View(bitacora);
+        }
+    }
+}
