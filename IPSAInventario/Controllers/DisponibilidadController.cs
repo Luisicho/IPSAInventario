a7e4583b8908961f41f110981a10bb79839d6b10
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-15 11:52:41 -0600

    addSavetoDisponibilidadController

--- IPSAInventario/Controllers/DisponibilidadController.cs
+++ IPSAInventario/Controllers/DisponibilidadController.cs
@@ -48,5 +48,37 @@ namespace IPSAInventario.Controllers
             };
             return View("DisponibilidadForm", newDisponibilidad);
         }
+        // GET: /Disponibilidad/Save
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Disponibilidad disponibilidad)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                var newDisponibilidad = new DisponibilidadFormViewModel(disponibilidad);
+                return View("DisponibilidadForm", newDisponibilidad);
+            }
+
+            if (disponibilidad.IdDisponibilidad == 0)
+            {
+                //agrega model a DB
+                _context.Disponibilidad.Add(disponibilidad);
+            }
+            else
+            {
+                var disponibilidadInDb = _context.Disponibilidad.Single(d => d.IdDisponibilidad == disponibilidad.IdDisponibilidad);
+                disponibilidadInDb.Computadora = disponibilidad.Computadora;
+                disponibilidadInDb.Area = disponibilidad.Area;
+                disponibilidadInDb.Departamento = disponibilidad.Departamento;
+                disponibilidadInDb.Ubicacion_Act = disponibilidad.Ubicacion_Act;
+                disponibilidadInDb.Status_ = disponibilidad.Status_;
+                disponibilidadInDb.Disponible = disponibilidad.Disponible;
+                disponibilidadInDb.Fecha_Asignacion = disponibilidad.Fecha_Asignacion;
+                disponibilidadInDb.Responsable = disponibilidad.Responsable;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Disponibilidad", new { id = disponibilidad.Codigo_PC.Trim() });
+        }
     }
 }
