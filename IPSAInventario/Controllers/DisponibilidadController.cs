eda041eb8de9b2b2c641ea68918ccd5079192264
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-15 09:31:42 -0600

    addDisponibilidadController

---
+++ IPSAInventario/Controllers/DisponibilidadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPSAInventario.Controllers
+{
+    public class DisponibilidadController : Controller
+    {
+        // GET: Disponibilidad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
