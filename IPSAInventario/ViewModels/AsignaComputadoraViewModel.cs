e0c2df3e1dd51026d34e32ef9453ff8dc3de0321
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-22 10:38:13 -0600

    modifyAsignaComputadoraViewModel

--- IPSAInventario/ViewModels/AsignaComputadoraViewModel.cs
+++ IPSAInventario/ViewModels/AsignaComputadoraViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace IPSAInventario.ViewModels
 {
     public class AsignaComputadoraViewModel
     {
+        public int IDFactura { get; set; }
+        public string Codigo_PC { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? Fecha { get; set; }
+
+        public TimeSpan? Hora { get; set; }
+
+        public string Responsable { get; set; }
+
     }
 }
