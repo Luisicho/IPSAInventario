8c583ea6f892eacd0e822f84f9f3772271548b3a
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-23 10:41:51 -0600

    addSaveToPerifericosController

--- IPSAInventario/Controllers/PerifericosController.cs
+++ IPSAInventario/Controllers/PerifericosController.cs
@@ -21,5 +21,19 @@ namespace IPSAInventario.Controllers
             var newPerifericos = new PerifericosFormViewModel(new Perifericos());
             return View("PerifericosForm", newPerifericos);
         }
+        //----------------------------------------Funciones de Controlador (CRUD)
+        // GET: Hardware/save
+        // Inserta un nuevo modelo a la base de datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Perifericos perifericos)
+        {
+            if (!ModelState.IsValid)
+            {
+                var nuevaVista = new PerifericosFormViewModel(perifericos);
+                return View("PerifericosForm", nuevaVista);
+            }
+            return RedirectToAction("Index","Perifericos");
+        }
     }
 }
