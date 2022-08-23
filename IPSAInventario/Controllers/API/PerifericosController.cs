3c619febc3f6cccbd841b29bc2936a59a575e651
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-23 09:28:38 -0600

    addPerifericosAPI

---
+++ IPSAInventario/Controllers/API/PerifericosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using IPSAInventario.Models;
+
+namespace IPSAInventario.Controllers.API
+{
+    public class PerifericosController : ApiController
+    {
+        private InventarioDbcontext _context;
+        public PerifericosController()
+        {
+            _context = new InventarioDbcontext();
+        }
+        // GET: /api/perifericos
+        [HttpGet]
+        public IHttpActionResult GetPerifericos()
+        {
+            var perifericos = _context.Perifericos.ToList();
+            return Ok(perifericos);
+        }
+    }
+}
