47ed314c25b4cfc8f1a22d15af11de6116ba6137
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-24 10:38:16 -0600

    modifyMonitorFormView

--- IPSAInventario/ViewModels/MonitorFormViewModel.cs
+++ IPSAInventario/ViewModels/MonitorFormViewModel.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using IPSAInventario.Models;
 
 namespace IPSAInventario.ViewModels
 {
     public class MonitorFormViewModel
     {
+        public MonitorFormViewModel() { }
+        public MonitorFormViewModel(Monitor monitor) 
+        {
+            IDPeriferico = monitor.IDPeriferico;
+            IDMonitor = monitor.IDMonitor;
+            Tipo = monitor.Tipo;
+            Tamano = monitor.Tamano;
+            Unidad_Med = monitor.Unidad_Med;
+        }
+
+        [Key]
+        [StringLength(7)]
+        public string IDPeriferico { get; set; }
+
+        public string IDMonitor { get; set; }
+
+        public string Tipo { get; set; }
+
+        public int? Tamano { get; set; }
+
+        public string Unidad_Med { get; set; }
     }
 }
