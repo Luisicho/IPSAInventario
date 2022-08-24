dc4632d88623a614eafd68e4609e6199e5feb295
Luisicho <lumihernadezma@ittepic.edu.mx>
2022-08-24 11:38:18 -0600

    addDisplayToImpresora

--- IPSAInventario/Models/Impresora.cs
+++ IPSAInventario/Models/Impresora.cs
@@ -12,13 +12,13 @@ namespace IPSAInventario.Models
         [Key]
         [StringLength(7)]
         public string IDPeriferico { get; set; }
-
+        [Display(Name = "Cartucho Negro")]
         public string Cart_Negro { get; set; }
-
+        [Display(Name = "Cartucho de Color")]
         public string Cart_Color { get; set; }
 
         public string Tipo { get; set; }
-
+        [Display(Name = "Tipo de Impresora")]
         public string Tipo_Imp { get; set; }
 
         public string Responsable { get; set; }
